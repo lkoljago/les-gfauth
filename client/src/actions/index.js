9381377653bdb93574b751aeda54b8300ebcf88f
lkoljago <l.koljago@gmail.com>
2020-01-04 14:06:27 +0300

    small add

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,22 +1,37 @@
 import axios from 'axios';
+import { AUTH_SIGN_UP, AUTH_ERROR } from './types';
 
 /*
     ActionCreators => create/return Actions({ }) => dispatched => middlewares => reducers
 */
 export const signUp = data => {
     /*
-      step 1: use the data and to make http request to BE and end to along
-      step 2: Take the BE's response (JWTToken!)
-      step 3: dispatch user just signed up (with JWTToken)
-      step 4: save JWTToken into clients localStorage
+      step 1: use the data and to make http request to BE and end to along [x]
+      step 2: Take the BE's response (JWTToken!) [x]
+      step 3: dispatch user just signed up (with JWTToken) [x]
+      step 4: save JWTToken into clients localStorage [x]
     */
   return async dispatch => {
     try {
+      console.log('[ActionCreator] SignUp is called!');
+      
       const res = await axios.post('http://localhost:9000/users/signup', data);
       console.log('res ', res);
+
+      console.log('[ActionCreator] SignUp dispatched an action');
+      
+      dispatch({
+        type: AUTH_SIGN_UP,
+        payload: res.data.token
+      })
+
+      localStorage.setItem('JWT_TOKEN', res.data.token);
       
     } catch(err) {
-      console.log('err ', err);
+      dispatch({
+        type: AUTH_ERROR,
+        payload: 'Email is already in use'
+      })
       
     }  
   }
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,3 +1,5 @@
+import { AUTH_SIGN_UP, AUTH_ERROR } from '../actions/types';
+
 const DEFAULT_STATE = {
   isAuthontificated: false,
   token: '',
@@ -5,5 +7,15 @@ const DEFAULT_STATE = {
 }
 
 export default (state = DEFAULT_STATE, action) => {
+  switch(action.type) {
+    case AUTH_SIGN_UP:
+      console.log('[AuthReducer] got an AUTH_SIGN_UP in action');
+      return { ...state, token: action.payload, isAuthontificated: true, errorMessage: '' }
+    case AUTH_ERROR:
+      console.log('[AuthReducer] got an AUTH_ERROR in action');
+      return { ...state, errorMessage: action.payload }
+    default:
+      return state
+  }
   return state;
 }
